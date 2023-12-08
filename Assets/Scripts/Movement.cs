ddaf04b8d34105aaf08f5aaab5602d9552369ef3
Fatih Aykut <aykut3005@gmail.com>
2023-12-09 00:49:54 +0300

    update "d" ınteract

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject canvas1;
 
     private void Start()
     {
         canvas.SetActive(false);
+        canvas1.SetActive(false);
     }
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.A))
         {
             Invoke("OpenCanvas", 1.5f);
-            Debug.Log("Çalışşşş köle");
         }
         else
         {
             canvas.SetActive(false);
         }
+    
+        if (Input.GetKey(KeyCode.D))
+        {
+            Invoke("OpenCanvas1", 1.5f);
+        }
+        else
+        {
+            canvas1.SetActive(false);
+        }
     }
+
 
     private void OpenCanvas()
     {
         canvas.SetActive(true);
+    }
+
+    private void OpenCanvas1()
+    {
+        canvas1.SetActive(true);
     }
 }
