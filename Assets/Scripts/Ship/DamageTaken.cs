3766c67d69dca1127aa964ce2cbcc9614fdfaf85
Berkay Tunca <berkaytunca40@gmail.com>
2023-12-09 10:59:21 +0300

    Issue: Enemy does not despawn.

--- Assets/Scripts/Ship/DamageTaken.cs
+++ Assets/Scripts/Ship/DamageTaken.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class DamageTaken : MonoBehaviour
 {
     private int ShipHealth = 100;
-    // TODO: On collision with respective object, the object such as meteors do not respawn.
+
+    [SerializeField] private GameObject bigMeteorPrefab;
+    [SerializeField] private GameObject meteorPrefab;
+    [SerializeField] private GameObject enemyPrefab;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("BigMeteor"))
@@ -22,7 +26,7 @@ public class DamageTaken : MonoBehaviour
         else if (collision.CompareTag("Enemy"))
         {
             ShipHealth -= 30;
-            Destroy(collision.gameObject);
+            DestroyImmediate(collision.gameObject);
             Debug.Log("Ship Health -30 New Health = " + ShipHealth);
         }
     }
