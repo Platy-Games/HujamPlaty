d4e6cf3e45d41b6f96a1e8f0907e5f09c4e009a5
Yunus Emre Bayram <emree_bayram@hotmail.com>
2023-12-09 03:13:22 +0300

    our ship attack
    mouse ile tıkladığımız yöne doğru mermi gönderen kod

---
+++ Assets/bulletScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public float DestroyTime;
+    public float speed;
+    Vector3 targetPos;
+    Vector3 direction;
+    private void Start()
+    {
+        Destroy(gameObject,DestroyTime);
+
+
+        targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPos.z = 0f;
+
+        direction = (targetPos - transform.position).normalized;
+    }
+    private void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
