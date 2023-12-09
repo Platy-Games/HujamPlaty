396c85b74954410d0c0eaa93e89e6cdbf326c414
tunaeskanaytac <tunaeskanaytac@gmail.com>
2023-12-09 14:58:49 +0300

    cooldown

--- Assets/Scripts/playerAttack.cs
+++ Assets/Scripts/playerAttack.cs
@@ -1,15 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerAttack : MonoBehaviour
+public class PlayerAttack : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform spawnPos;
+    [SerializeField] private float cooldown;
+    private void Start()
+    {
+        _isCooldownOver = true;
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && _isCooldownOver)
         {
+            StartCoroutine(SetCooldown());
             CreateABullet();
         }
 
@@ -17,5 +25,13 @@ public class playerAttack : MonoBehaviour
     void CreateABullet()
     {
         GameObject instance = Instantiate(bulletPrefab, spawnPos.position, Quaternion.identity);
+    }
+
+    private bool _isCooldownOver;
+    private IEnumerator SetCooldown()
+    {
+        _isCooldownOver = false;
+        yield return new WaitForSeconds(cooldown);
+        _isCooldownOver = true;
     }
 }
