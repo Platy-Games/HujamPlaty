65490ac514d2c8961a070b7b172fc7051763744e
Fatih Aykut <aykut3005@gmail.com>
2023-12-09 06:39:50 +0300

    Weapen animation Fixed

--- Assets/Scripts/playerAttack.cs
+++ Assets/Scripts/playerAttack.cs
@@ -6,16 +6,28 @@ public class playerAttack : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform spawnPos;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator.GetComponent<Animator>();
+        float fire = 0f;
+    }
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             CreateABullet();
+            float fire = 1f;
+            animator.SetFloat("Fire", fire);
+            
         }
 
     }
     void CreateABullet()
     {
         GameObject instance = Instantiate(bulletPrefab, spawnPos.position, Quaternion.identity);
+        float fire = 3f;
+        animator.SetFloat("Fire", fire);
     }
 }
