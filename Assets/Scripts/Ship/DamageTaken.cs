1a19300eff3ca92cc5cea195ceb80697a7999f4c
Fatih Aykut <aykut3005@gmail.com>
2023-12-09 04:44:00 +0300

    DamageTaken.cs Update

---
+++ Assets/Scripts/Ship/DamageTaken.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTaken : MonoBehaviour
+{
+    [SerializeField] private GameObject BigMeteor;
+    [SerializeField] private GameObject Meteor;
+    [SerializeField] private GameObject EnemyBullets;
+    [SerializeField] private GameObject Enemy;
+
+    private int ShipHealth = 100;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("BigMeteor"))
+        {
+            ShipHealth -= 50;
+        }
+        if (other.CompareTag("Meteor"))
+        {
+            ShipHealth -= 15;
+        }
+        if (other.CompareTag("Enemy"))
+        {
+            ShipHealth -= 30;
+        }
+        if (other.CompareTag("EnemyBullets"))
+        {
+            ShipHealth -= 20;
+        }
+    }
+}
