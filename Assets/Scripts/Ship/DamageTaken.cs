e7901e04187caec522220af1cd39f3a6069e19df
Berkay Tunca <berkaytunca40@gmail.com>
2023-12-09 13:38:18 +0300

    .

--- Assets/Scripts/Ship/DamageTaken.cs
+++ Assets/Scripts/Ship/DamageTaken.cs
@@ -16,18 +16,19 @@ public class DamageTaken : MonoBehaviour
         {
             ShipHealth -= 25;
             Destroy(collision.gameObject);
+            Debug.Log("Ship Health -25 New Health = " + ShipHealth);
         }
         else if (collision.CompareTag("Meteor"))
         {
             ShipHealth -= 15;
             Destroy(collision.gameObject);
-            //Debug.Log("Ship Health -15 New Health = " + ShipHealth);
+            Debug.Log("Ship Health -15 New Health = " + ShipHealth);
         }
         else if (collision.CompareTag("Enemy"))
         {
             ShipHealth -= 30;
             Destroy(collision.gameObject);
-            //Debug.Log("Ship Health -30 New Health = " + ShipHealth);
+            Debug.Log("Ship Health -30 New Health = " + ShipHealth);
         }
     }
     /*if (other.CompareTag("EnemyBullets"))
