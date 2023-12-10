8636859b0c3440de8f64ac9790e76d0b240cfae0
Fatih Aykut <aykut3005@gmail.com>
2023-12-11 02:36:50 +0300

    upgradeManager Update

--- Assets/Scripts/Ship/DamageTaken.cs
+++ Assets/Scripts/Ship/DamageTaken.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class DamageTaken : MonoBehaviour
 {
-    private int ShipHealth = 100;
+    public int ShipHealth = 100;
 
     [SerializeField] private GameObject bigMeteorPrefab;
     [SerializeField] private GameObject meteorPrefab;
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private GameObject EnemyBulletsPrefab;
+    
+    private UpgradeManager upgradeManager;
+
+    public int upgradeLevel = 0;
+
+    private void Start()
+    {
+        upgradeManager = FindObjectOfType<UpgradeManager>();
+
+        ApplyUpgrades1();
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -35,6 +46,14 @@ public class DamageTaken : MonoBehaviour
             ShipHealth -= 30;
             Destroy(collision.gameObject);
             Debug.Log("Ship Health -30 New Health = " + ShipHealth);
+        }
+    }
+
+    public void ApplyUpgrades1()
+    {
+        if (upgradeManager != null)
+        {
+            upgradeManager.ApplyUpgrades1(this);
         }
     }
 }
--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -6,9 +6,14 @@ public class UpgradeManager : MonoBehaviour
 {
     public float damageupgrade;
     public float speedupgrade;
+    public int healthupgrade;
     public void ApplyUpgrades(bulletScript bulletScript)
     {
         bulletScript.bulletStrength += damageupgrade;
         bulletScript.speed += speedupgrade;
+    }
+    public void ApplyUpgrades1(DamageTaken damagetaken)
+    {
+        damagetaken.ShipHealth += healthupgrade;
     }
 }
