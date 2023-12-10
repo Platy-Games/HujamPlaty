32eb16aaa1685fde54772841d1917cd6c430c19e
Fatih Aykut <aykut3005@gmail.com>
2023-12-11 00:30:36 +0300

    Ganca Update

--- Assets/Scripts/Meteor/MeteorBehaviour.cs
+++ Assets/Scripts/Meteor/MeteorBehaviour.cs
@@ -6,24 +6,41 @@ using UnityEngine;
 public class MeteorBehaviour : MonoBehaviour
 {
     private GameObject ganca;
+
     private void Start()
     {
-        _isHooked = false;
-        GANCA.HookedEvent.AddListener(Hooked);
+        // 1. Ganka Objesini Bul
         ganca = GameObject.Find("Hook");
+
+        // 2. Ganka Var mı Kontrol Et
+        if (ganca != null)
+        {
+            // 3. Ganka Bulunamazsa Hata Ver
+            GANCA.HookedEvent.AddListener(Hooked);
+        }
+        else
+        {
+            Debug.LogError("GANCA objesi bulunamadı.");
+        }
+
+        // 4. İlk Durumu Ayarla
+        _isHooked = false;
     }
 
     private void Update()
     {
         if (_isHooked)
         {
+            // Ganka'ya doğru hareket et
             transform.position = Vector3.MoveTowards(transform.position, ganca.transform.position, 0.03f);
         }
     }
 
     private bool _isHooked;
+
     private void Hooked()
     {
+        // Ganka yakalandığında _isHooked'i true yap
         _isHooked = true;
     }
 }
