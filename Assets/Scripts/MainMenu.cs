0610921b5bf164ab246d470d7b92218234407f39
Yunus Emre Bayram <emree_bayram@hotmail.com>
2023-12-11 04:33:06 +0300

    main menu
    yapildi renk ayarinde esseklik yapmis olabilirim ama calisiyo bide ses yok ve endless mod scenesi yok bide story mod scenesi eklenmemiş bu kadar

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,87 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEditor.Progress;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    GameObject options;
+    GameObject start;
+    GameObject help;
+    GameObject title;
+   
+    private void Start()
     {
-        SceneManager.LoadSceneAsync("SampleScene");
-    }
+        options = GameObject.Find("options");
+        start = GameObject.Find("start");
+        title = GameObject.FindGameObjectWithTag("title");
+        help=GameObject.Find("help");
+        options.SetActive(false);
+        start.SetActive(false);
+        help.SetActive(false);
 
-    public void QuitGame()
+    }
+    public void butonSec(int gelenButon)
     {
-        Application.Quit();
+        if (gelenButon==1)
+        {
+            start.SetActive(true);
+            title.SetActive(false);
+            options.SetActive(false);
+            help.SetActive(false);
+
+        }
+        else if (gelenButon == 2)
+        {
+            title.SetActive(false);
+            options.SetActive(true);
+            help.SetActive(false);
+            start.SetActive(false);
+
+        }
+        else if (gelenButon == 3)
+        {
+            title.SetActive(false);
+            help.SetActive(true);
+            start.SetActive(false);
+            options.SetActive(false);
+        }
+        else if (gelenButon == 4)
+        {
+            Application.Quit();
+        }
+        else if (gelenButon ==5)
+        {
+            title.SetActive(true);
+            options.SetActive(false);
+        }
+        else if (gelenButon == 6)
+        {
+            //ses ac
+        }
+        else if (gelenButon == 7)
+        {
+            //ses kapa
+        }
+        if (gelenButon==8) 
+        {
+                SceneManager.LoadSceneAsync("SampleScene"); 
+        }
+        else if (gelenButon == 9)
+        {
+            //SceneManager.LoadSceneAsync("EndlessScane"); daha yapýlmadý
+        }
+        else if (gelenButon == 10)
+        {
+            title.SetActive(true);
+            start.SetActive(false);
+        }
+        else if (gelenButon == 11)
+        {
+            title.SetActive(true);
+            help.SetActive(false);
+        }
+
+
     }
 }
